da137e7a23ee3ecbad746cfc72bd270f14449897
Benjamin Torben-Nielsen <btorbennielsen@gmail.com>
2016-07-21 08:56:33 +0100

    Change __init__.py for correct loading

--- btmorph2/__init__.py
+++ btmorph2/__init__.py
@@ -4,4 +4,7 @@ from btstructs import Tree
 from btstructs import Node
 from btstructs import P3D
 
+#from tools.swc_to_hdf5 import swc_to_ntf
+#from tools.swc_to_hdf5 import curate_ntf
+
 from grids import VoxelGrid
