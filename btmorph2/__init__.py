782898d1c7e7af3e3b2a5abf84aac1450e27e28e
Benjamin Torben-Nielsen <btorbennielsen@gmail.com>
2017-09-27 21:45:05 +0200

    After rebase. Change __init__.py. All tests work

--- btmorph2/__init__.py
+++ btmorph2/__init__.py
@@ -1,15 +1,15 @@
-from btstructs import NeuronMorphology
-from btstructs import PopulationMorphology
-from btstructs import Tree
-from btstructs import Node
-from btstructs import P3D
+from .btstructs import NeuronMorphology
+from .btstructs import PopulationMorphology
+from .btstructs import Tree
+from .btstructs import Node
+from .btstructs import P3D
 
 # For NeuroMac back-compatibility
-from btstructs import P3D as P3D2
-from btstructs import Node as SNode2
-from btstructs import Tree as STree2 
+from .btstructs import P3D as P3D2
+from .btstructs import Node as SNode2
+from .btstructs import Tree as STree2 
 
 #from tools.swc_to_hdf5 import swc_to_nmf
 #from tools.swc_to_hdf5 import curate_nmf
 
-from grids import VoxelGrid
+from .grids import VoxelGrid
